7f3736b7aff91c5c09ad8401eff23ef358ece1e9
Konstantin V <kostiantyn.voiku@gmail.com>
2024-08-26 10:34:34 +0300

    init commit

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress"
+import cypressSplit from 'cypress-split'
+
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      cypressSplit(on, config)
+    }
+  },
+  watchForFileChanges: false,
+  chromeWebSecurity: false,
+})
---
+++ cypress/e2e/spec1.cy.ts
@@ -0,0 +1,18 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('waits and passes', () => {
+    cy.wait(1000)
+  })
+
+  it('waits and passes', () => {
+    cy.wait(1000)
+  })
+
+  it('waits and passes', () => {
+    cy.wait(1000)
+  })
+
+})
